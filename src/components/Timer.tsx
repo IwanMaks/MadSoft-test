e53c817ed768bc7c7ef942659459cc18115f247c
Iwan Maksackov <iwan.maksackov@yandex.ru>
2024-06-17 17:49:24 +0300

    add timer logic

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+
 export const Timer = () => {
+  const [timeLeft, setTimeLeft] = useState<number>(60 * 15); // 20 minutes
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeLeft((prevTime) => (prevTime > 0 ? prevTime - 1 : 0));
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  const formatTime = (seconds: number) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes < 10 ? "0" + minutes : minutes}:${
+      remainingSeconds < 10 ? "0" + remainingSeconds : remainingSeconds
+    }`;
+  };
+
   return (
     <div className="timer">
-      <p className="font-semibold px-3 py-1 border border-black rounded-md">15:46</p>
+      <p className="font-semibold px-3 py-1 border border-black rounded-md">
+        {formatTime(timeLeft)}
+      </p>
     </div>
   );
 };
